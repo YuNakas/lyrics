5add30438d999bb03eb3cb22505444e2530ddadc
YuNakas <metal.edge.core.ease@gmail.com>
2023-12-16 19:55:35 +0900

    ファイルコピー用のプログラムを作成

--- source/moveFiles.js
+++ source/moveFiles.js
@@ -1,10 +1,17 @@
 import fs from "fs";
-import songList from "./src/assets/songList.json"
+import songList from "./src/assets/songList.json" with {type: "json"};
 
-songList.map((song) => {
-    fs.mkdirSync('src/assets/lyrics');
-    fs.mkdirSync('src/assets/lyrics/' + song.category);
-    song.song.map((songName)=>{
-        fs.copyFileSync('lyrics/' + song.category + '/' + songName + '.txt', 'src/assets/lyrics/' + song.category + '/' + songName + '.md')
-    })
-})
+export const copyFiles = () => {
+    fs.mkdirSync("src/assets/lyrics");
+  songList.map((song) => {
+    fs.mkdirSync("src/assets/lyrics/" + song.category);
+    song.song.map((songName) => {
+      fs.copyFileSync(
+        "../lyrics/" + song.category + "/" + songName + ".txt",
+        "src/assets/lyrics/" + song.category + "/" + songName + ".md"
+      );
+    });
+  });
+};
+
+copyFiles()

4357321733faaafc40669319e1e4ffb73e6f854a
YuNakas <metal.edge.core.ease@gmail.com>
2024-10-19 13:00:32 +0900

    styleの変更

--- source/tailwind.config.mjs
+++ source/tailwind.config.mjs
@@ -1,8 +1,16 @@
+import defaultTheme from 'tailwindcss/defaultTheme'
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		fontFamily: {
+			'sans': ['Noto Sans JP Variable', ...defaultTheme.fontFamily.sans],
+		},
 		extend: {},
+	},
+	daisyui: {
+		themes: ["lemonade"]
 	},
 	plugins: [
 		require('daisyui'),
